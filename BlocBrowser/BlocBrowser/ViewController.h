80997687fcb37267cccb80677bdfa2dcb12b903b
jkral <jeff@sandboxbrands.com>
2016-08-11 18:03:18 -0400

    functioning web browser

---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Jeff Kral on 8/11/16.
+//  Copyright © 2016 Jeff Kral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
