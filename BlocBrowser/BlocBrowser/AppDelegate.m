f7bfc5030a7284b03347beb9d92d05111ac64a0c
jkral <jeff@sandboxbrands.com>
2016-08-12 14:05:58 -0400

    assignment 24

--- BlocBrowser/BlocBrowser/AppDelegate.m
+++ BlocBrowser/BlocBrowser/AppDelegate.m
@@ -22,6 +22,17 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    
+    UIAlertView *alertView = [[UIAlertView alloc]
+                              initWithTitle:@"BlocBrowser"
+                              message:@"Welcome to BlocBrowser"
+                              delegate:self
+                              cancelButtonTitle:@"Cancel"
+                              otherButtonTitles:@"OK", nil];
+    
+    [alertView show];
+
+    
 
     return YES;
 }
@@ -29,6 +40,10 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    UINavigationController *navigationVC = (UINavigationController *)self.window.rootViewController;
+    ViewController *browserVC = [[navigationVC viewControllers] firstObject];
+    [browserVC resetWebView];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
