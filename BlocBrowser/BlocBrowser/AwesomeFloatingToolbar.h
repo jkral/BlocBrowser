cf16f3e1189b677900427bea35d1a5189393e162
jkral <jeff@sandboxbrands.com>
2016-08-14 11:22:23 -0400

    assignment 25

---
+++ BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,32 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Jeff Kral on 8/12/16.
+//  Copyright © 2016 Jeff Kral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+@interface AwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
+
+
+
